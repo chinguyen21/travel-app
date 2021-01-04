01e584b795428703d69d9fce8826549b89aa97fc
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-01-04 18:04:50 -0500

    session_part_next

--- app/controllers/event_entries_controller.rb
+++ app/controllers/event_entries_controller.rb
@@ -1,2 +1,21 @@
 class EventEntriesController < ApplicationController
+
+    def new
+        @event_entry = EventEntry.new
+    end
+
+    def create 
+        @event_entry = EvenEntry.new(event_entry_params)
+        if @event_entry.save
+            redirect_to @event_entry 
+        else
+            flash[:errors] = @event_entry.errors.full_messages
+            render "new"
+        end
+    end
+
+    private
+    def event_entry_params
+        params.require(:event_entry).permit(:event_id, :entry_id)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 Destination.destroy_all
 
 d1 = Destination.create(name: "McDonald Observatory", city: "Fort Davis", state: "Texas", country: "United States", picture_link: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fnews.rice.edu%2Ffiles%2F2016%2F05%2F0525_PLANET-UTMcD-233-lg-15kwd14.jpg&f=1&nofb=1")
+
+
+e1 = Event.create(name: "Star Party", price: 100, destination_id: d1.id)
+e2 = Event.create(name: "Tour", price: 20, destination_id: d1.id)
