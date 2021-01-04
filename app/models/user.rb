9df893275d43fce4fca84ee70d10a87a3f36a020
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-01-04 16:03:52 -0500

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ApplicationRecord
     has_many :entries, through: :itineraries
     has_many :event_entries, through: :entries
     has_many :events, through: :event_entries
+
+
+   
+    validates :name, presence: true
+    validates :age, presence: true
+    validates :email, presence: true
+    validates :password, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: {minimum: 8}
+    has_secure_password
+
 end
